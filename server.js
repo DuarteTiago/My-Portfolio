9b7b5afa1b9f494b241caccdeaa8b98b58f15dce
TiagoDuarte <tiago_sdv2@hotmail.com>
2023-03-24 16:24:38 -0300

    inserção do path para o frontend no arquivo server.js

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    succes: true,
-  });
-});
-
 app.use("/api", api);
+
+if (process.env.Node_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  const path = require(path);
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT);
