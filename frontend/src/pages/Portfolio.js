cb6d47b512e076bc7fcb967f8f9d3a8f1dadffa9
TiagoDuarte <tiago_sdv2@hotmail.com>
2023-03-31 14:31:55 -0300

    implementação em portfolio para receber portfiloDetail usando o useLocation do react router dom

--- frontend/src/pages/Portfolio.js
+++ frontend/src/pages/Portfolio.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 import styled from "styled-components";
+import { useLocation } from "react-router-dom";
 
 import Card from "../components/Card";
+
+import PortfolioDetail from "../components/PortfolioDetail";
 
 import { useApi } from "../hooks/useApi";
 
 const Portfolio = () => {
+  const location = useLocation();
+  const slug = location.pathname.split("/")[2];
   const { data } = useApi("/portfolio");
   return (
     <PortfolioList>
+      {slug && <PortfolioDetail slug={slug} />}
       <CardList>
         {data?.data?.map((project) => {
           return <Card key={project.slug} project={project} />;
