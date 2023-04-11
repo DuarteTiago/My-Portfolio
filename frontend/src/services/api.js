1b12d2d3c0e1aa654c771633d3ad44155722701c
TiagoDuarte <tiago_sdv2@hotmail.com>
2023-04-11 14:33:32 -0300

    criação das funçães CRUD com o mongoBD, usadas na área adm do site

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,3 +5,60 @@ const api = axios.create({
 });
 
 export default api;
+
+export const deleteItem = (slug) => {
+  axios
+    .delete(`/api/portfolio/${slug}`)
+    .then((res) => {
+      console.log("Resultado", res);
+    })
+    .catch((e) => {
+      console.log("Erro", e);
+      return e;
+    });
+};
+
+export const editItem = (
+  slug,
+  { title, description, longDescription, image, technologies }
+) => {
+  axios
+    .patch(`/api/portfolio/${slug}`, {
+      title,
+      description,
+      longDescription,
+      image,
+      technologies,
+    })
+    .then((res) => {
+      console.log("Alaterado", res);
+    })
+    .catch((e) => {
+      console.log("Erro", e);
+      return e;
+    });
+};
+
+export const addItem = ({
+  title,
+  description,
+  longDescription,
+  image,
+  technologies,
+}) => {
+  axios
+    .post("/api/portfolio", {
+      title,
+      description,
+      longDescription,
+      image,
+      technologies,
+    })
+    .then((res) => {
+      console.log("Adicionado", res);
+    })
+    .catch((e) => {
+      console.log("Erro", e);
+      return e;
+    });
+};
